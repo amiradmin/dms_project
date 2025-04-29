9ba6c7036ea222b0eb0c7f961e9ee35462ea9245
Amir <amirbehvandi747@gmail.com>
2025-04-29 13:17:08 +0330

    Adding Nginx

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']  # Change this for production
 
 # Application definition
 INSTALLED_APPS = [
@@ -78,17 +78,16 @@ CACHES = {
 
 # Static files
 STATIC_URL = '/static/'
-
-STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Only needed for development
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
+# Additional locations the staticfiles app will traverse
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
+# Media files (uploads, etc.)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # DRF settings
 REST_FRAMEWORK = {
@@ -116,3 +115,123 @@ AWS_S3_ENDPOINT_URL = 'http://minio:9000'
 AWS_S3_USE_SSL = False
 AWS_S3_VERIFY = False
 AWS_QUERYSTRING_AUTH = False
+
+
+# import os
+# from pathlib import Path
+#
+# BASE_DIR = Path(__file__).resolve().parent.parent
+#
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+#
+# DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+#
+
+# ALLOWED_HOSTS = ['yourdomain.com', 'localhost', '127.0.0.1']
+#
+# # Application definition
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'rest_framework',
+#     'rest_framework.authtoken',
+#     'storages',
+#     'documents',
+#     'drf_yasg',  # for swagger
+#     'users'
+# ]
+#
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+#
+# ROOT_URLCONF = 'config.urls'
+#
+# TEMPLATES = [{
+#     'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#     'DIRS': [],
+#     'APP_DIRS': True,
+#     'OPTIONS': {
+#         'context_processors': [
+#             'django.template.context_processors.debug',
+#             'django.template.context_processors.request',
+#             'django.contrib.auth.context_processors.auth',
+#             'django.contrib.messages.context_processors.messages',
+#         ],
+#     },
+# }]
+#
+# WSGI_APPLICATION = 'config.wsgi.application'
+#
+# # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_DB', 'dms_db'),
+#         'USER': os.environ.get('POSTGRES_USER', 'dms_user'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'dms_password'),
+#         'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+#         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+#     }
+# }
+#
+# # Redis cache
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': 'redis://redis:6379/1',
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#         }
+#     }
+# }
+#
+# # Static files
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#
+# # Additional locations the staticfiles app will traverse
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+#
+# # Media files (uploads, etc.)
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#
+# # DRF settings
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.TokenAuthentication',
+#         'rest_framework.authentication.SessionAuthentication',
+#         'rest_framework.authentication.BasicAuthentication',
+#     ],
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ],
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 10,
+# }
+#
+# AUTH_USER_MODEL = 'users.CustomUser'
+#
+# # MinIO / S3 Storage Settings
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#
+# AWS_ACCESS_KEY_ID = 'minioadmin'
+# AWS_SECRET_ACCESS_KEY = 'minioadmin'
+# AWS_STORAGE_BUCKET_NAME = 'dms-bucket'
+# AWS_S3_ENDPOINT_URL = 'http://minio:9000'
+# AWS_S3_USE_SSL = False
+# AWS_S3_VERIFY = False
+# AWS_QUERYSTRING_AUTH = False
